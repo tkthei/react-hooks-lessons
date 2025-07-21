7296dd78ff23cda165b15d8b587b0b36ebccbf07
tkt <mypc.igosso.no.18@gmail.com>
2025-07-21 13:42:24 +0900

    lesson2-2 end

--- src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
+++ src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchBio } from "./fetchBio";
+
 const Lesson2_2 = () => {
+  const [person, setPerson] = useState("ShinCode");
+  const [bio, setBio] = useState<string | null>(null);
+
+  useEffect(() => {
+    let ignore = false;
+    const startFetching = async () => {
+      const bio = await fetchBio(person);
+      if (ignore) return;
+      setBio(bio);
+    };
+
+    startFetching();
+
+    return () => {
+      ignore = true;
+    };
+  }, [person]);
+
   return (
     <div>
-      <select>
+      <select onChange={(e) => setPerson(e.target.value)} value={person}>
         <option value="ShinCode">ShinCode</option>
         <option value="TestUser">TestUser</option>
         <option value="SampleUser">SampleUser</option>
@@ -9,7 +30,7 @@ const Lesson2_2 = () => {
 
       <hr />
 
-      <p className="text-black">{"Loading..."}</p>
+      <p className="text-black">{bio ?? "Loading..."}</p>
     </div>
   );
 };
