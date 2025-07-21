faf4b645c2e5338eb45f96b3bdeb8b1464419e90
tkt <mypc.igosso.no.18@gmail.com>
2025-07-21 12:02:59 +0900

    lesson 1-1 end

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const Lesson1_1 = () => {
+  const [age, setAge] = useState(0);
   return (
     <div>
       <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <button
+        className="border p-2 rounded-md bg-red-100"
+        onClick={() => setAge(age + 1)}
+      >
+        Add Age
+      </button>
+      <p>You are {age}</p>
     </div>
   );
 };
